97785efce743d64cb1bf5d17e3860de72c4d15bd
shrivatsa-r-joshi <shrivatsarjoshi2007@gmail.com>
2025-08-29 20:43:31 +0530

    Day1

---
+++ Day1.py
@@ -0,0 +1,36 @@
+t = (1,2,2,2,3,4,5)
+s = {1,2,2,2,3,4,5}
+print(s)
+print(t)
+
+print(type(10))
+print(type(9.8)) # Floats
+print(type(3.14)) #floats
+print(type(['Sgriat'])) # set
+print(type(10))
+
+# Writing different types of Python Data types 
+s1 = {1,2,2,4,5}
+t1 = (1,2,3,4,2,5)
+d = {
+    'name' : 'shirvatsa',
+    'class' : '11'    
+}
+l = ['Shrivatsa', 'Joshi', 'R']
+b = True
+c = 5 - 5j
+print(type(s1))
+print(type(t1))
+print(type(d))
+print(type(l))
+print(type(b))
+print(type(c))
+
+# Finding the euclidean distance
+p1 = 2
+p2 = 3
+q1 = 10
+q2 = 8
+r1 = (p1-q1) ** 2
+r2 = (p2-q1) ** 2
+print('Euclidaen Distance:',  (r1 + r2) ** 0.5 )
